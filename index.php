56b8247bbe786737031d2e1e1b4f7bfb7ff2c252
Alexander Kotov <amikotov@gmail.com>
2018-02-03 19:31:49 +0300

    Use arrays for storing lots data

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+include('lotsdata.php');
+
 $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
@@ -29,7 +31,7 @@ $user_avatar = 'img/user.jpg';
         <nav class="user-menu">
             <?php if ($is_auth): ?>
                 <div class="user-menu__image">
-                    <img src=<?=$user_avatar; ?> width="40" height="40" alt="Пользователь">
+                    <img src="<?= $user_avatar; ?>" width="40" height="40" alt="<`?= $user_name; ?>">
                 </div>
                 <div class="user-menu__logged">
                     <p><?=$user_name; ?></p>
@@ -78,24 +80,28 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
+            <?php foreach ($lots as $lot): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?= $lot['image_url']; ?>" width="350" height="260" alt="<?= $lot['title']; ?>">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?= $lot['category']; ?></span>
+                        <h3 class="lot__title">
+                            <a class="text-link" href="lot.html"><?= $lot['title']; ?></a>
+                        </h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?= $lot['cost']; ?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
 
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            <?php endforeach; unset($lot); ?>
         </ul>
     </section>
 </main>
@@ -103,24 +109,11 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+            <?php foreach ($lots_categories as $category): ?>
+                <li class="nav__item">
+                    <a href="all-lots.html"><?= $category ?></a>
+                </li>
+            <?php endforeach; unset($category); ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
---
+++ lotsdata.php
@@ -0,0 +1,42 @@
+<?php
+  $lots_categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
+  $lots =[
+    [
+      "title" => "2014 Rossignol District Snowboard",
+      "category" => $lots_categories[0],
+      "cost" => 10999,
+      "image_url" => "img/lot-1.jpg"
+    ],
+    [
+      "title" => "DC Ply Mens 2016/2017",
+      "category" => $lots_categories[0],
+      "cost" => 159999,
+      "image_url" => "img/lot-2.jpg"
+    ],
+    [
+      "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
+      "category" => $lots_categories[1],
+      "cost" => 8000,
+      "image_url" => "img/lot-3.jpg"
+    ],
+    [
+      "title" => "Ботинки для сноуборда DC Mutiny Charocal",
+      "category" => $lots_categories[2],
+      "cost" => 10999,
+      "image_url" => "img/lot-4.jpg"
+    ],
+    [
+      "title" => "Куртка для сноуборда DC Mutiny Charocal",
+      "category" => $lots_categories[3],
+      "cost" => 7500,
+      "image_url" => "img/lot-5.jpg"
+    ],
+    [
+      "title" => "Маска Oakley Canopy",
+      "category" => $lots_categories[5],
+      "cost" => 5400,
+      "image_url" => "img/lot-6.jpg"
+    ]
+  ]
+?>
